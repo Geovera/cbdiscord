7e48d4e87b011e305bff7b270369d63a14acb9ba
Geovanny <geovera16@gmail.com>
2020-05-26 00:01:51 -0400

    Minor changes to settings path

--- server/settings.js
+++ server/settings.js
@@ -1,5 +1,5 @@
-env_path = process.env.ENV_PATH ? process.env.ENV_PATH : '';
-require('dotenv').config(env_path)
+env_path = process.env.ENV_PATH ? process.env.ENV_PATH : './.env';
+require('dotenv').config({path:env_path})
 SETTINGS = process.env;
 
 module.exports = SETTINGS;
