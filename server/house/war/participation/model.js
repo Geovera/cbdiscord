20ed25f088688de01ce58cc0a8c54803ebcd9cc4
Geovanny <geovera16@gmail.com>
2020-06-12 23:24:58 -0400

    Added last_updated column for getting and updating war participation

--- server/house/war/participation/model.js
+++ server/house/war/participation/model.js
@@ -12,8 +12,8 @@ model.getCurrentWar = async() => {
 
 model.warParticipation = async (user_id, house_id, decision) => {
     const sql_text = 'SELECT @current_war_id:=id FROM war_days WHERE completed = 0 LIMIT 1;';
-    const sql_text2 = `INSERT INTO users_war (user_id, war_id, house_id, decision) VALUES (?, @current_war_id, ?, ?) 
-                       ON DUPLICATE KEY UPDATE decision = ?`;
+    const sql_text2 = `INSERT INTO users_war (user_id, war_id, house_id, decision, last_updated) VALUES (?, @current_war_id, ?, ?, NOW()) 
+                       ON DUPLICATE KEY UPDATE decision = ?, last_updated = NOW()`;
 
     let con = await db.pool.getConnection();
 
@@ -32,7 +32,8 @@ model.getParticipation = async (house_id) => {
                       FROM users as u
                       LEFT JOIN users_war as uw ON uw.user_id = u.id
                       LEFT JOIN war_days as w ON w.id = uw.war_id
-                      WHERE uw.house_id = ? AND w.completed = 0;`;
+                      WHERE uw.house_id = ? AND w.completed = 0 
+                      ORDER BY uw.last_updated ASC;`;
 
     const data = await db.pool.query(sql_text, [house_id]);
     return data;
